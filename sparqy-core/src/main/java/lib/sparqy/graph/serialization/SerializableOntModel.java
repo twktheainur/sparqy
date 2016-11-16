d8f5be9db8c86518a0c0d3bf26173c6efc866c3b
Andon Tchechmedjiev <twk.theainur@gmail.com>
2016-11-16 18:55:53 +0100

    Initial code transfer from lexsema

---
+++ sparqy-core/src/main/java/lib/sparqy/graph/serialization/SerializableOntModel.java
@@ -0,0 +1,8 @@
+package lib.sparqy.graph.serialization;
+
+import com.hp.hpl.jena.ontology.OntModel;
+
+import java.io.Serializable;
+
+public interface SerializableOntModel extends OntModel, Serializable{
+}
