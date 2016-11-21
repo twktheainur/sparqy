e223a7fb3f327f7e89635ede0901388d34e74b46
twktheainur <twk.theainur@gmail.com>
2016-11-21 12:13:49 +0100

    Renamed package root name

--- sparqy-core/src/main/java/lib/sparqy/graph/serialization/SerializableOntModel.java
+++
@@ -1,8 +0,0 @@
-package lib.sparqy.graph.serialization;
-
-import com.hp.hpl.jena.ontology.OntModel;
-
-import java.io.Serializable;
-
-public interface SerializableOntModel extends OntModel, Serializable{
-}
