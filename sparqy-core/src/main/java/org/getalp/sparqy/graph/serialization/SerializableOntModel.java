c12cb49054131b3a1e309dc279c40f4d74a5e9ee
twktheainur <twk.theainur@gmail.com>
2016-11-22 14:13:02 +0100

    Renamed package root name again to match with the rules of the maven central repository

--- sparqy-core/src/main/java/org/getalp/sparqy/graph/serialization/SerializableOntModel.java
+++
@@ -1,8 +0,0 @@
-package org.getalp.sparqy.graph.serialization;
-
-import com.hp.hpl.jena.ontology.OntModel;
-
-import java.io.Serializable;
-
-public interface SerializableOntModel extends OntModel, Serializable{
-}
